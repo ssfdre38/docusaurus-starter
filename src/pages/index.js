b46290f02a9bd354a10849b824d6257bea02b8d0
Daniel Elliott <ssfdre38@msn.com>
2024-03-24 12:41:47 -0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/category/amp">
+            to="/docs/intro">
             Explore the Docs.
           </Link>
         </div>
