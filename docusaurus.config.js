d3ae0bf2985440f9fed10ef663aa87b18454e72a
Daniel Elliott <ssfdre38@msn.com>
2024-03-24 01:26:47 -0700

    updates

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -94,7 +94,7 @@ const config = {
             items: [
               {
                 label: 'Docs',
-                to: '/docs',
+                to: '/docs/category/amp',
               },
             ],
           },
